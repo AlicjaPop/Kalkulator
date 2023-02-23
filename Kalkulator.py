a86f7ed82af87b6101f3da8471a59f1e97effe54
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-02-23 22:08:11 +0100

    Saved file

--- Kalkulator.py
+++ Kalkulator.py
@@ -10,11 +10,11 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-text_1=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+operation=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
 num_1=float(input("Podaj pierwszą liczbę:"))
 num_2=float(input("Podaj drugą liczbę:"))
 
-for text in text_1:
+for text in operation:
     if text=="1":
         print("Dodaję liczby", num_1, "oraz", num_2)
         print("Wynik to:", add(num_1,num_2))
@@ -24,7 +24,7 @@ for text in text_1:
     elif text=="3":
         print("Mnożę liczbę", num_1, "przez liczbę", num_2)
         print("Wynik to:", multiply(num_1,num_2))
-    elif text=="4":
+    else:
         print("Dzielę liczbę", num_1, "przez", num_2)
         print("Wynik to:", divide(num_1,num_2))
 
