2b8d401bdca8f8bdcbf23c609fb922f2c654dbbb
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-02-24 12:21:23 +0100

    Calculator with logging

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,3 +1,6 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 def add(x, y):
     return x + y
 
@@ -11,20 +14,29 @@ def divide(x, y):
     return x / y
 
 operation=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-num_1=float(input("Podaj pierwszą liczbę:"))
-num_2=float(input("Podaj drugą liczbę:"))
+num_1=input("Podaj pierwszą liczbę:")
+num_2=input("Podaj drugą liczbę:")
 
 for text in operation:
     if text=="1":
-        print("Dodaję liczby", num_1, "oraz", num_2)
+        logging.debug(f"Dodaję liczby {num_1} oraz {num_2}")
+        num_1=float(num_1)
+        num_2=float(num_2) 
         print("Wynik to:", add(num_1,num_2))
     elif text=="2":
-        print("Odejmuję od liczby", num_1, "liczbę", num_2)
+        logging.debug(f"Odejmuję od liczby {num_1} liczbę {num_2}")
+        num_1=float(num_1)
+        num_2=float(num_2) 
         print("Wynik to:", subtract(num_1,num_2))
     elif text=="3":
-        print("Mnożę liczbę", num_1, "przez liczbę", num_2)
+        logging.debug(f"Mnożę liczbę {num_1} przez liczbę {num_2}")
+        num_1=float(num_1)
+        num_2=float(num_2) 
         print("Wynik to:", multiply(num_1,num_2))
     else:
-        print("Dzielę liczbę", num_1, "przez", num_2)
+        logging.debug(f"Dzielę liczbę {num_1} przez {num_2}")
+        num_1=float(num_1)
+        num_2=float(num_2)
         print("Wynik to:", divide(num_1,num_2))
 
+ 
