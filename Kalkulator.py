2ec8cbfca2c108927dd8d6a4762f46787b3a6dd4
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-02-25 17:42:46 +0100

    Final code

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,12 +1,6 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def subtract(x, y):
-    return x - y
-
-def divide(x, y):
-    return x / y
-
 operation=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
 
 for text in operation:
@@ -21,7 +15,7 @@ for text in operation:
             logging.debug(f"Dodaję liczby {num_list}")
             print(f"Wynik to: {result}.")
         except ValueError:
-            logging.debug("Podano nieprawidłową wartość.")
+            logging.debug("Podano nieprawidłową wartość - należy podać liczbę.")
             continue
     elif text=="2":
         num_1=input("Podaj pierwszą liczbę:")
@@ -30,12 +24,12 @@ for text in operation:
             num_1=float(num_1)
             num_2=float(num_2)
             logging.debug(f"Odejmuję od liczby {num_1} liczbę {num_2}")
-            print("Wynik to:", subtract(num_1,num_2))
+            print("Wynik to:", num_1-num_2)
         except ValueError:
-            logging.debug("Podano nieprawidłową wartość.")
+            logging.debug("Podano nieprawidłową wartość - należy podać liczbę.")
             continue
     elif text=="3":
-        num_text=input("Podaj liczby, które chcesz przez siebie pomonożyć, rozgraniczając je przecinkiem:")
+        num_text=input("Podaj liczby, które chcesz przez siebie pomnożyć, rozgraniczając je przecinkiem:")
         try:
             num_list=num_text.split(",")
             result=1
@@ -45,7 +39,7 @@ for text in operation:
             logging.debug(f"Mnożę liczby {num_list}.") 
             print(f"Wynik to: {result}.")
         except ValueError:
-            logging.debug("Podano nieprawidłową wartość.")
+            logging.debug("Podano nieprawidłową wartość - należy podać liczbę.")
             continue
     elif text=="4":
         num_1=input("Podaj pierwszą liczbę:")
@@ -54,9 +48,9 @@ for text in operation:
             num_1=float(num_1)
             num_2=float(num_2)
             logging.debug(f"Dzielę liczbę {num_1} przez {num_2}")
-            print("Wynik to:", divide(num_1,num_2))
+            print("Wynik to:", num_1/num_2)
         except ValueError:
-            logging.debug("Podano nieprawidłową wartość.")
+            logging.debug("Podano nieprawidłową wartość - należy podać liczbę.")
             continue
     else:
         print("Podano nieprawidłową liczbę.")
